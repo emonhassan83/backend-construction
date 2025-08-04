69f8ab5951719c2966261a4f9d47a4386e3c5984
Emon <emonhasan7650@gmail.com>
2025-08-04 20:31:07 +0600

    notification module added

---
+++ src/app/modules/notification/notification.constant.ts
@@ -0,0 +1,32 @@
+export const messages = {
+  adminProfile: {
+    passwordChanged: 'Password changed successfully.',
+    passwordReset: 'Password Reset Processed',
+    passwordForgot: 'Password Forgot Processed',
+  },
+
+  userManagement: {
+    createAccount: 'User account created.',
+    accountActivated: 'User account activated.',
+    accountDeactivated: 'User account deactivated.',
+  },
+
+  purityMeter: {
+    empty: 'Purity Meter Empty',
+    warning: 'Purity Meter Running Low',
+  },
+
+  trackWater: {
+    completed: 'You finally reached you goal !',
+    inCompleted: 'You were not able to reach you goal !',
+  },
+
+  settings: {
+    systemUpdated: 'System settings updated.',
+  },
+
+  paymentManagement: {
+    paymentSuccess: 'Payment was successful!',
+    paymentRefunded: 'Payment refunded successfully.',
+  },
+}
