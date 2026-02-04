f48c68ece06d349c8af14a6d555377942b2716ab
emonhassan83 <emonhasan7650@gmail.com>
2026-02-04 13:06:38 +0600

    payment customer create

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,9 @@ import { WorkPhotoControllers } from './app/modules/workPhotos/workPhotos.contro
 const app: Application = express()
 
 //* parser
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: '500mb' }));
+app.use(express.urlencoded({ limit: '500mb', extended: true }));
+
 app.use(
    cors({
     origin: true,
